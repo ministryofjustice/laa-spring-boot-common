9cd710695fe57dba4f7f8da6db0bc7f5d5b39a0a
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2024-12-09 16:43:56 +0000

    CCMSPUI-424 | Add govuk-dialect spring boot starter

---
+++ laa-ccms-spring-boot-starters/laa-ccms-spring-boot-starter-govuk-dialect/src/main/java/uk/gov/laa/ccms/springboot/dialect/GovUkThymeleafDialectConfig.java
@@ -0,0 +1,22 @@
+package uk.gov.laa.ccms.springboot.dialect;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+/**
+ * Custom ThymeleafDialectConfig.
+ */
+@AutoConfiguration
+@ConditionalOnClass(SpringTemplateEngine.class)
+public class GovUkThymeleafDialectConfig {
+  /**
+   * SpringTemplateEngine with custom dialect.
+   */
+  @Bean
+  public GovUkDialect govUkDialect() {
+    return new GovUkDialect();
+  }
+
+}
