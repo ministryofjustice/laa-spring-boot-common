03630b189659c22a741b3afc612f6b8ac9b163ca
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-12-03 16:19:22 +0000

    Added tests

--- laa-spring-boot-starters/laa-spring-boot-starter-metrics/src/main/java/uk/gov/laa/springboot/metrics/service/AbstractMetricService.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-metrics/src/main/java/uk/gov/laa/springboot/metrics/service/AbstractMetricService.java
@@ -5,6 +5,7 @@ import io.prometheus.metrics.model.registry.PrometheusRegistry;
 import java.util.Arrays;
 import java.util.HashMap;
 import org.springframework.stereotype.Component;
+import uk.gov.laa.springboot.metrics.exception.MetricsException;
 
 /**
  * A generic abstract class designed to manage metrics of a specific type. The class is
@@ -19,6 +20,7 @@ public abstract class AbstractMetricService<T extends Metric> {
 
   protected final PrometheusRegistry prometheusRegistry;
   protected final HashMap<String, T> metrics = new HashMap<>();
+  protected final HashMap<String, String[]> metricLabels = new HashMap<>();
 
   protected AbstractMetricService(PrometheusRegistry prometheusRegistry) {
     this.prometheusRegistry = prometheusRegistry;
@@ -30,18 +32,28 @@ public abstract class AbstractMetricService<T extends Metric> {
    * Registers a metric with the Prometheus registry.
    *
    * @param metricName the name of the metric to register
-   * @param help the help text for the metric
-   * @param labels the labels for the metric
+   * @param help       the help text for the metric
+   * @param labels     the labels for the metric
    */
   public void register(String metricName, String help, String... labels) {
-    String[] labelNames =
+    String[] labelNamesFromAnnotation =
         Arrays.stream(labels).map(x -> x.split("=")[0]).toList()
             .toArray(String[]::new);
-    T metric = buildMetric(metricName, help, labelNames);
+    if (metrics.containsKey(metricName) && !Arrays.equals(
+        metricLabels.get(metricName), labelNamesFromAnnotation)) {
+      throw new MetricsException(
+          ("Labels must match for metric %s. Please ensure that all uses of this annotation and "
+              + "metric name contain the same label names").formatted(
+              metricName));
+    }
+
+    T metric = buildMetric(metricName, help, labelNamesFromAnnotation);
     metrics.put(metricName, metric);
+    metricLabels.put(metricName, labelNamesFromAnnotation);
   }
 
   public T getMetric(String metricName) {
     return metrics.get(metricName);
   }
+
 }
