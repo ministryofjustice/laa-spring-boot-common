5dca1280e5b6a564ba8a9709d27a7428ebb6a38d
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-12-05 18:28:11 +0000

    Rename HistogramMetric and SummaryMetric to HistogramTimerMetric and SummaryTimerMetric

--- laa-spring-boot-starters/laa-spring-boot-starter-metrics/src/main/java/uk/gov/laa/springboot/metrics/aspect/annotations/HistogramTimerMetric.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-metrics/src/main/java/uk/gov/laa/springboot/metrics/aspect/annotations/HistogramTimerMetric.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface SummaryMetric {
+public @interface HistogramTimerMetric {
 
   /**
    * The name of the metric you wish to record. Will be appended to the base metric name.
