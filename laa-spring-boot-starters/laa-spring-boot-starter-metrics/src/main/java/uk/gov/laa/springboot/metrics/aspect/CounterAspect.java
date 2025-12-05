26347ac376047b4ba1ed79b5dc5d38d5963d3793
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-12-05 18:53:59 +0000

    Added new histogram annotation to record return values

--- laa-spring-boot-starters/laa-spring-boot-starter-metrics/src/main/java/uk/gov/laa/springboot/metrics/aspect/CounterAspect.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-metrics/src/main/java/uk/gov/laa/springboot/metrics/aspect/CounterAspect.java
@@ -1,6 +1,7 @@
 package uk.gov.laa.springboot.metrics.aspect;
 
 import io.micrometer.common.util.StringUtils;
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -11,7 +12,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 import uk.gov.laa.springboot.metrics.aspect.annotations.CounterMetric;
 import uk.gov.laa.springboot.metrics.aspect.annotations.CounterMetrics;
+import uk.gov.laa.springboot.metrics.aspect.annotations.HistogramMetric;
+import uk.gov.laa.springboot.metrics.aspect.annotations.HistogramMetrics;
 import uk.gov.laa.springboot.metrics.service.CounterMetricService;
+import uk.gov.laa.springboot.metrics.service.HistogramMetricService;
 
 /**
  * Aspect to track counter metrics.
@@ -25,6 +29,7 @@ import uk.gov.laa.springboot.metrics.service.CounterMetricService;
 public class CounterAspect {
 
   private final CounterMetricService counterMetricService;
+  private final HistogramMetricService histogramMetricService;
 
   /**
    * Counts up a metric after method has returned.
@@ -61,6 +66,42 @@ public class CounterAspect {
   public void countUpMultiple(CounterMetrics counters, Object result) {
     for (CounterMetric metric : counters.value()) {
       countUp(metric, result);
+    }
+  }
+
+
+  /**
+   * Counts up a metric after method has returned.
+   *
+   * @param histogramMetric the histogramMetric annotation
+   * @param result          the method return value
+   */
+  @AfterReturning(value = "@annotation(histogramMetric)", returning = "result")
+  public void countUpHistogram(HistogramMetric histogramMetric, Object result) {
+    if (result == null) {
+      return;
+    }
+    String metricName = histogramMetric.metricName();
+    List<String> labelsList = new java.util.ArrayList<>(
+        Arrays.stream(histogramMetric.labels()).map(x -> x.split("=")[1]).toList());
+
+    BigDecimal value = new BigDecimal(result.toString());
+    histogramMetricService.recordValue(
+        metricName, value.doubleValue(), labelsList.toArray(String[]::new));
+    log.debug("Incremented histogramMetric {} by {}", metricName, value.doubleValue());
+  }
+
+  /**
+   * Counts up a metric after method has returned. Handles methods with multiple histograms
+   * annotations.
+   *
+   * @param histograms the histograms collection annotation
+   * @param result   the method return value
+   */
+  @AfterReturning(value = "@annotation(histograms)", returning = "result")
+  public void countUpMultipleHistograms(HistogramMetrics histograms, Object result) {
+    for (HistogramMetric metric : histograms.value()) {
+      countUpHistogram(metric, result);
     }
   }
 
