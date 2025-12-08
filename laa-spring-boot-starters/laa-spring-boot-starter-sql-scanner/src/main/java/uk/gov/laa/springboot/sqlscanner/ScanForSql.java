e1e523cb47e12ec9fa608ed1cac892d3571463a3
NagarjunaRachakonda <Nagarjuna.Rachakonda@justice.gov.uk>
2025-12-08 17:11:30 +0000

    Include ElementType.Parameter to place the ScanForSql annotation

--- laa-spring-boot-starters/laa-spring-boot-starter-sql-scanner/src/main/java/uk/gov/laa/springboot/sqlscanner/ScanForSql.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-sql-scanner/src/main/java/uk/gov/laa/springboot/sqlscanner/ScanForSql.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * Marks a type, field, or record component for SQL-like pattern scanning.
  */
-@Target({ElementType.TYPE, ElementType.FIELD, ElementType.RECORD_COMPONENT, ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.RECORD_COMPONENT, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ScanForSql {
 }
