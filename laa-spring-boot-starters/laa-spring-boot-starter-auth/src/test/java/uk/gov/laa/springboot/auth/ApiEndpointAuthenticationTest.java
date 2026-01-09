3b0ed14135efede58648c0ffcd59e3c40de60c01
Phil Payne <phil.payne@digital.justice.gov.uk>
2026-01-09 14:40:10 +0000

    Add additional tests

--- laa-spring-boot-starters/laa-spring-boot-starter-auth/src/test/java/uk/gov/laa/springboot/auth/ApiEndpointAuthenticationTest.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-auth/src/test/java/uk/gov/laa/springboot/auth/ApiEndpointAuthenticationTest.java
@@ -7,9 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -119,6 +122,26 @@ class ApiEndpointAuthenticationTest {
     mockMvc.perform(get("/resource3/specific").header(HttpHeaders.AUTHORIZATION,
             "f6c85ef8-2525-4e6c-9b45-5899f65d5b78"))
         .andExpect(status().is(HttpStatus.NO_CONTENT.value()));
+  }
+
+  @Test
+  void testStreamingEndpointAsyncDispatchPreservesAuth() throws Exception {
+    MvcResult result = mockMvc.perform(
+            get("/resource1/streaming-group1").header(HttpHeaders.AUTHORIZATION,
+                "b7bbdb3d-d0b9-4632-b752-b2e0f9486baf"))
+        .andExpect(request().asyncStarted())
+        .andReturn();
+
+    mockMvc.perform(asyncDispatch(result))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void testStreamingEndpointWithoutRoleForbidden() throws Exception {
+    mockMvc.perform(
+            get("/resource1/streaming-group1").header(HttpHeaders.AUTHORIZATION,
+                "1fd84ad9-760d-401f-8cf0-7a80aa42566c"))
+        .andExpect(status().is(HttpStatus.FORBIDDEN.value()));
   }
 
 }
