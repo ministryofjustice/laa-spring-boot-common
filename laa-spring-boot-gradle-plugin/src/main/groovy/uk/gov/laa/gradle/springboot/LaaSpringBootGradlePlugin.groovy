41210ccea7cdce5e8e9600c97fc6b228b2c57c90
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2026-02-10 14:53:06 +0000

    Fix issue with dependency - Could not get unknown property 'dependencyProject'

--- laa-spring-boot-gradle-plugin/src/main/groovy/uk/gov/laa/gradle/springboot/LaaSpringBootGradlePlugin.groovy
+++ laa-spring-boot-gradle-plugin/src/main/groovy/uk/gov/laa/gradle/springboot/LaaSpringBootGradlePlugin.groovy
@@ -4,7 +4,6 @@ import io.spring.gradle.dependencymanagement.DependencyManagementPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
-import org.gradle.api.artifacts.ProjectDependency
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
 import uk.gov.laa.gradle.LaaJavaGradlePlugin
 import uk.gov.laa.gradle.springboot.starter.export.SpringBootStarterExportCodegenTasks
@@ -49,13 +48,7 @@ class LaaSpringBootGradlePlugin implements Plugin<Project> {
     if (dep == null) {
       return false
     }
-    if (dep.group == 'uk.gov.laa.springboot' && dep.name == 'laa-spring-boot-starter-export') {
-      return true
-    }
-    if (dep instanceof ProjectDependency) {
-      def projectName = dep.dependencyProject?.name
-      return projectName == 'laa-spring-boot-starter-export'
-    }
-    return false
+    return dep.name == 'laa-spring-boot-starter-export'
+        && (dep.group == null || dep.group == 'uk.gov.laa.springboot')
   }
 }
