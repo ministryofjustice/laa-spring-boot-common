5b15d3a73671655a32b74cda0f2dd59770edf6d0
drummondjm <mark.drummond@justice.gov.uk>
2026-03-02 12:40:09 +0000

    initial

---
+++ laa-spring-boot-starters/laa-spring-boot-starter-observability/src/main/java/uk/gov/laa/springboot/observability/config/ObservabilityAutoConfiguration.java
@@ -0,0 +1,48 @@
+package uk.gov.laa.springboot.observability.config;
+
+import co.elastic.logging.logback.EcsEncoder;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@AutoConfiguration
+@EnableConfigurationProperties(ObservabilityProperties.class)
+@ConditionalOnProperty(
+        prefix = "laa.springboot.starter.observability",
+        name = "enabled",
+        havingValue = "true"
+)
+public class ObservabilityAutoConfiguration {
+
+  public ObservabilityAutoConfiguration(ObservabilityProperties properties) {
+    configureEcsLogging(properties);
+  }
+
+  private void configureEcsLogging(ObservabilityProperties properties) {
+    LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+    // ECS Encoder
+    EcsEncoder ecsEncoder = new EcsEncoder();
+    ecsEncoder.setServiceName(properties.getServiceName());
+    ecsEncoder.setServiceVersion(properties.getServiceVersion());
+    ecsEncoder.setServiceEnvironment(properties.getEnvironment());
+    ecsEncoder.setContext(context);
+    ecsEncoder.start();
+
+    // Console appender
+    ConsoleAppender<ILoggingEvent> consoleAppender = new ConsoleAppender<>();
+    consoleAppender.setContext(context);
+    consoleAppender.setEncoder(ecsEncoder);
+    consoleAppender.start();
+
+    // Attach to root logger
+    Logger rootLogger = context.getLogger(Logger.ROOT_LOGGER_NAME);
+    rootLogger.detachAndStopAllAppenders();
+    rootLogger.addAppender(consoleAppender);
+  }
+}
---
+++ laa-spring-boot-starters/laa-spring-boot-starter-observability/src/main/java/uk/gov/laa/springboot/observability/config/ObservabilityProperties.java
@@ -0,0 +1,29 @@
+package uk.gov.laa.springboot.observability.config;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "laa.springboot.starter.observability")
+public class ObservabilityProperties {
+
+  /** ECS logging configurable via property */
+  @NotBlank(message = "enabled is required")
+  private Boolean enabled;
+
+  /** The name of the service */
+  @NotBlank(message = "serviceName is required")
+  private String serviceName;
+
+  /** The version of the service */
+  @NotBlank(message = "serviceVersion is required")
+  private String serviceVersion;
+
+  /** The environment of the service */
+  @NotBlank(message = "environment is required")
+  private String environment;
+
+}
