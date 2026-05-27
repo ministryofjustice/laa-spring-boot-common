9b8eb569919bc6b1d5c01d0b5130d0600a8bccdc
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-05-27 16:05:18 +0100

    remove @component from interceptor

--- laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentAutoConfiguration.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentAutoConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,7 +13,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Auto-configuration for Cookie starter.
  */
 @AutoConfiguration
-@Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication
 @EnableConfigurationProperties(CookieConsentProperties.class)
 @ConditionalOnProperty(
