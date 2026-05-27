57de56a1988cca08db022195a1cc73ad3e4a12bf
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-05-27 16:15:43 +0100

    debug

--- laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentInterceptor.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentInterceptor.java
@@ -23,6 +23,7 @@ public class CookieConsentInterceptor implements HandlerInterceptor {
   public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) {
+      System.out.println("INTERCEPTOR RUNNING!");
     boolean analyticsConsented = false;
     boolean bannerSeen = false;
     boolean bannerHidden = false;
