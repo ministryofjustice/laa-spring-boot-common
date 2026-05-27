24b42b67bc89927252093d81a94cee82cf310274
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-05-27 17:40:09 +0100

    remove implements web mvc directly

--- laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentAutoConfiguration.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentAutoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,13 +22,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         havingValue = "true",
         matchIfMissing = true
 )
-public class CookieConsentAutoConfiguration implements WebMvcConfigurer {
+public class CookieConsentAutoConfiguration{
   private final CookieConsentProperties properties;
 
   public CookieConsentAutoConfiguration(CookieConsentProperties properties) {
     this.properties = properties;
   }
 
+  @Configuration
+  static class CookieConsentMvcConfiguration implements WebMvcConfigurer {
+      private final CookieConsentInterceptor interceptor;
+      CookieConsentMvcConfiguration(CookieConsentInterceptor interceptor) {
+          this.interceptor = interceptor;
+      }
+
+      @Override
+      public void addInterceptors(InterceptorRegistry registry) {
+          registry.addInterceptor(interceptor).addPathPatterns("/**");
+      }
+
+  }
   @Bean
   @ConditionalOnMissingBean
   public CookieConsentInterceptor cookieConsentInterceptor() {
@@ -40,8 +54,4 @@ public class CookieConsentAutoConfiguration implements WebMvcConfigurer {
     return new CookieConsentController(properties);
   }
 
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(cookieConsentInterceptor()).addPathPatterns("/**");
-  }
 }
