2837ffc4c96a06ed0d7759c7c7ec5b7fffa0e8a0
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-05-27 18:58:19 +0100

    logs

--- laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentInterceptor.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentInterceptor.java
@@ -5,9 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
