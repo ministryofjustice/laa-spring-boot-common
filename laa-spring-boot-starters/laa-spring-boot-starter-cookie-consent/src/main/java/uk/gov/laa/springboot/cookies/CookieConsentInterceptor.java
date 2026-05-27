53052fb220babe6092e40dcc3c6057c0e3cdd9c3
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-05-27 15:58:29 +0100

    remove @component from interceptor

--- laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentInterceptor.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentInterceptor.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 /**
  * Interceptor for Cookie consent.
  */
-@Component
 public class CookieConsentInterceptor implements HandlerInterceptor {
   private final CookieConsentProperties properties;
 
