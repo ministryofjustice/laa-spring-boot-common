b78f742d24e35ab3b5005293721f8c57a0600d78
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-05-28 10:17:54 +0100

    only leave component scan

--- laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentAutoConfiguration.java
+++ laa-spring-boot-starters/laa-spring-boot-starter-cookie-consent/src/main/java/uk/gov/laa/springboot/cookies/CookieConsentAutoConfiguration.java
@@ -1,13 +1,10 @@
 package uk.gov.laa.springboot.cookies;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 /**
  * Auto-configuration for Cookie starter.
@@ -23,9 +20,4 @@ import org.springframework.context.annotation.Import;
         matchIfMissing = true
 )
 public class CookieConsentAutoConfiguration {
-  private final CookieConsentProperties properties;
-
-  public CookieConsentAutoConfiguration(CookieConsentProperties properties) {
-    this.properties = properties;
-  }
 }
